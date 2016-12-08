bd6ef27d4adcc395ed1d47ace089cd3934e8eb9f
Wilko <wolki@binaryflakes.fritz.box>
2016-12-08 19:05:30 +0100

    initial commit of the backup script

---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Purpose = /home backup script with sync to gdrive
+#Created on 12-08-2016
+#Author = @wolki
+#Version 0.1
+
+#Config Backup
+TIMESTAMP=`date +%d-%b-%Y-%H-%M`
+BACKUPNAME=$HOSTNAME-$TIMESTAMP.zip #Backup Fileformat
+CREDS=`date +%s | sha256sum | base64 | head -c 64 ; echo` 
+LOCATION=/home/$USER/ #Location which should be backuped
+
+#Config Telegram
+USERID="" #Telegram Channel or Contact ID
+KEY="" #Botkey
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+IPADDR=$(curl ipecho.net/plain ; echo)
+TEXT="$HOSTNAME (Public: $IPADDR) backuped $LOCATION to Google Drive.
+
+$CREDS to decrypt $BACKUPNAME." #Telegram Message Text
+
+#Backing Up Process
+zip -r $BACKUPNAME $LOCATION -P $CREDS #Create the backup as a password protected .zip file
+gdrive upload $BACKUPNAME #upload backup to google drive
+rm $BACKUPNAME #remove local file when upload has finished
+curl -s -d "chat_id=$USERID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null #send message via telegram
